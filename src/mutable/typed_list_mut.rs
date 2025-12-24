cbde9be02ff38dd9c2a6a2274b70274e8aeec68a
zijinshanren <x_zijinshanren_x@outlook.com>
2025-12-24 22:22:07 +0800

    fix

--- src/mutable/typed_list_mut.rs
+++ src/mutable/typed_list_mut.rs
@@ -1,6 +1,6 @@
 use std::{marker::PhantomData, ptr};
 
-use crate::{ByteOrder, MutVec, NBT, TagID};
+use crate::{ByteOrder, MutVec, NBT};
 
 pub struct MutTypedList<'s, T: NBT, O: ByteOrder> {
     data: MutVec<'s, u8>,
@@ -8,7 +8,6 @@ pub struct MutTypedList<'s, T: NBT, O: ByteOrder> {
 }
 
 pub struct MutTypedListIter<'s, T: NBT, O: ByteOrder> {
-    tag_id: TagID,
     remaining: u32,
     data: *mut u8,
     _marker: PhantomData<(&'s (), O, T)>,
@@ -17,7 +16,6 @@ pub struct MutTypedListIter<'s, T: NBT, O: ByteOrder> {
 impl<'s, T: NBT, O: ByteOrder> Default for MutTypedListIter<'s, T, O> {
     fn default() -> Self {
         Self {
-            tag_id: TagID::End,
             remaining: 0,
             data: ptr::null_mut(),
             _marker: PhantomData,
