ecd639f83f2cb25fc3fa56672c18fd423be6cb3f
zijinshanren <x_zijinshanren_x@outlook.com>
2025-12-31 13:38:46 +0800

    fix bound checking

--- src/ser_de/de.rs
+++ src/ser_de/de.rs
@@ -1007,12 +1007,13 @@ impl<'a, 'de, O: ByteOrder> MapAccess<'de> for CompoundAccess<'a, 'de, O> {
     where
         K: de::DeserializeSeed<'de>,
     {
-        check_bounds!(1 + 2, self.deserializer.input);
+        check_bounds!(1, self.deserializer.input);
         self.deserializer.current_tag = TagID::from_u8(self.deserializer.input[0])?;
         if self.deserializer.current_tag.is_end() {
             self.deserializer.input = &self.deserializer.input[1..];
             return Ok(None);
         }
+        check_bounds!(1 + 2, self.deserializer.input);
         let name_len = byteorder::U16::<O>::from_bytes(unsafe {
             *self.deserializer.input.as_ptr().add(1).cast()
         })
